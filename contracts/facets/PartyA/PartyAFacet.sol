caec510a07c14c09fa381c2cdea3cd71f7885358
Navid <mojtaba.farokhi99@gmail.com>
2023-06-13 23:42:36 +0330

    Fix contract licenses

--- contracts/facets/PartyA/PartyAFacet.sol
+++ contracts/facets/PartyA/PartyAFacet.sol
@@ -1,4 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: SYMM-Core-Business-Source-License-1.1
+// This contract is licensed under the SYMM Core Business Source License 1.1
+// Copyright (c) 2023 Symmetry Labs AG
+// For more information, see https://docs.symm.io/legal-disclaimer/license
 pragma solidity >=0.8.18;
 
 import "./PartyAFacetImpl.sol";
