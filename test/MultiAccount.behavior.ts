b52df9d5575ae0834ef94fe794a0da56a4c62af4
zigBalthazar <zigbalthazar@gmail.com>
2023-12-31 19:31:11 +0330

    typo: layer to MultiAccount

--- test/MultiAccount.behavior.ts
+++ test/MultiAccount.behavior.ts
@@ -22,7 +22,7 @@ function getFunctionAbi(contract: Contract | ContractFactory, functionName: stri
 
 
 export function shouldBehaveLikMultiAccount() {
-  let layer: any;
+  let multiAccount: any;
   let context: RunContext;
   let user: User;
   let hedger: Hedger;
@@ -45,23 +45,23 @@ export function shouldBehaveLikMultiAccount() {
 
     const Factory = await ethers.getContractFactory("MultiAccount");
 
-    const Layer = await upgrades.deployProxy(
+    const MultiAccount = await upgrades.deployProxy(
       Factory,
       [await context.signers.admin.getAddress(), symmioAddress, SymmioPartyA.bytecode],
       { initializer: "initialize" },
     );
 
-    layer = await Layer.deployed();
+    multiAccount = await MultiAccount.deployed();
   });
 
   describe("Initialization and Settings", function() {
     it("Should set the correct admin and Symmio address", async function() {
         const adminAddress = await context.signers.admin.getAddress();
-        expect(await layer.hasRole(await layer.DEFAULT_ADMIN_ROLE(), adminAddress)).to.equal(true);
-        expect(await layer.hasRole(await layer.PAUSER_ROLE(), adminAddress)).to.equal(true);
-        expect(await layer.hasRole(await layer.UNPAUSER_ROLE(), adminAddress)).to.equal(true);
-        expect(await layer.hasRole(await layer.SETTER_ROLE(), adminAddress)).to.equal(true);
-        expect(await layer.symmioAddress()).to.equal(symmioAddress);
+        expect(await multiAccount.hasRole(await multiAccount.DEFAULT_ADMIN_ROLE(), adminAddress)).to.equal(true);
+        expect(await multiAccount.hasRole(await multiAccount.PAUSER_ROLE(), adminAddress)).to.equal(true);
+        expect(await multiAccount.hasRole(await multiAccount.UNPAUSER_ROLE(), adminAddress)).to.equal(true);
+        expect(await multiAccount.hasRole(await multiAccount.SETTER_ROLE(), adminAddress)).to.equal(true);
+        expect(await multiAccount.symmioAddress()).to.equal(symmioAddress);
       });
   
       describe("Role-based Access Control", function() {
@@ -71,21 +71,21 @@ export function shouldBehaveLikMultiAccount() {
           const adminAddress = await context.signers.admin.getAddress();
     
           // Granting SETTER_ROLE to addr1
-          await layer.grantRole(await layer.SETTER_ROLE(), userAddress);
-          expect(await layer.hasRole(await layer.SETTER_ROLE(), userAddress)).to.equal(true);
+          await multiAccount.grantRole(await multiAccount.SETTER_ROLE(), userAddress);
+          expect(await multiAccount.hasRole(await multiAccount.SETTER_ROLE(), userAddress)).to.equal(true);
     
           // Revoking SETTER_ROLE from addr1
-          await layer.revokeRole(await layer.SETTER_ROLE(), userAddress, { from: adminAddress });
-          expect(await layer.hasRole(await layer.SETTER_ROLE(), userAddress)).to.equal(false);
+          await multiAccount.revokeRole(await multiAccount.SETTER_ROLE(), userAddress, { from: adminAddress });
+          expect(await multiAccount.hasRole(await multiAccount.SETTER_ROLE(), userAddress)).to.equal(false);
         });
     
         it("Should not allow unauthorized access", async function() {
           // Trying to call a protected function from an unauthorized address
-          await expect(layer.connect(context.signers.user).setAccountImplementation("0x00")).to.be.reverted;
+          await expect(multiAccount.connect(context.signers.user).setAccountImplementation("0x00")).to.be.reverted;
     
           // Granting SETTER_ROLE to addr2 and trying again
-          await layer.grantRole(await layer.SETTER_ROLE(), await context.signers.user.getAddress());
-          await expect(layer.connect(context.signers.user).setAccountImplementation("0x00")).to.not.be.reverted;
+          await multiAccount.grantRole(await multiAccount.SETTER_ROLE(), await context.signers.user.getAddress());
+          await expect(multiAccount.connect(context.signers.user).setAccountImplementation("0x00")).to.not.be.reverted;
         });
       });
   })
@@ -95,24 +95,24 @@ export function shouldBehaveLikMultiAccount() {
       it("Should create account", async function() {
         const userAddress = await context.signers.user.getAddress();
 
-        expect(await layer.getAccountsLength(userAddress)).to.be.equal(0);
+        expect(await multiAccount.getAccountsLength(userAddress)).to.be.equal(0);
 
-        await layer.connect(context.signers.user).addAccount("Test");
+        await multiAccount.connect(context.signers.user).addAccount("Test");
 
-        expect(await layer.getAccountsLength(userAddress)).to.be.equal(1);
-        let createdAccount = (await layer.getAccounts(userAddress, 0, 10))[0];
+        expect(await multiAccount.getAccountsLength(userAddress)).to.be.equal(1);
+        let createdAccount = (await multiAccount.getAccounts(userAddress, 0, 10))[0];
         expect(createdAccount.name).to.be.equal("Test");
-        expect(await layer.owners(createdAccount.accountAddress)).to.be.equal(userAddress);
+        expect(await multiAccount.owners(createdAccount.accountAddress)).to.be.equal(userAddress);
       });
 
       it("Should edit account name", async function() {
         const userAddress = await context.signers.user.getAddress();
-        await expect(layer.connect(context.signers.user).addAccount("Test")).to.not.be.reverted;
+        await expect(multiAccount.connect(context.signers.user).addAccount("Test")).to.not.be.reverted;
 
-        let createdAccount = (await layer.getAccounts(userAddress, 0, 10))[0];
-        await expect(layer.connect(context.signers.user2).editAccountName(createdAccount.accountAddress, "Renamed")).to.be.reverted;
-        await layer.connect(context.signers.user).editAccountName(createdAccount.accountAddress, "Renamed");
-        let renamedAccount = (await layer.getAccounts(userAddress, 0, 10))[0];
+        let createdAccount = (await multiAccount.getAccounts(userAddress, 0, 10))[0];
+        await expect(multiAccount.connect(context.signers.user2).editAccountName(createdAccount.accountAddress, "Renamed")).to.be.reverted;
+        await multiAccount.connect(context.signers.user).editAccountName(createdAccount.accountAddress, "Renamed");
+        let renamedAccount = (await multiAccount.getAccounts(userAddress, 0, 10))[0];
         expect(renamedAccount.name).to.be.equal("Renamed");
       });
 
@@ -124,33 +124,33 @@ export function shouldBehaveLikMultiAccount() {
         beforeEach(async function() {
           const userAddress = await context.signers.user.getAddress();
     
-          await layer.connect(context.signers.user).addAccount("Test");
-          partyAAccount = (await layer.getAccounts(userAddress, 0, 10))[0].accountAddress;
+          await multiAccount.connect(context.signers.user).addAccount("Test");
+          partyAAccount = (await multiAccount.getAccounts(userAddress, 0, 10))[0].accountAddress;
 
           await context.collateral.connect(context.signers.user).mint(userAddress, decimal(120))
     
           await context.collateral
             .connect(context.signers.user)
-            .approve(layer.address, ethers.constants.MaxUint256);
+            .approve(multiAccount.address, ethers.constants.MaxUint256);
         });
     
         it("Should deposit for account", async () => {
           //partyA
-          await layer.connect(context.signers.user).depositForAccount(partyAAccount, decimal(100));
+          await multiAccount.connect(context.signers.user).depositForAccount(partyAAccount, decimal(100));
           expect(await context.viewFacet.balanceOf(partyAAccount)).to.be.equal(decimal(100));
 
         });
     
         it("Should deposit and allocate for account partyA", async () => {
-          await layer.connect(context.signers.user).depositAndAllocateForAccount(partyAAccount, decimal(100));
+          await multiAccount.connect(context.signers.user).depositAndAllocateForAccount(partyAAccount, decimal(100));
           expect((await context.viewFacet.balanceInfoOfPartyA(partyAAccount))[0]).to.be.equal(decimal(100));
         });
     
         it("Should withdraw from account", async () => {
           //partyA
-          await layer.connect(context.signers.user).depositForAccount(partyAAccount, decimal(100));
+          await multiAccount.connect(context.signers.user).depositForAccount(partyAAccount, decimal(100));
           expect(await context.viewFacet.balanceOf(partyAAccount)).to.be.equal(decimal(100));
-          await layer.connect(context.signers.user).withdrawFromAccount(partyAAccount, decimal(50));
+          await multiAccount.connect(context.signers.user).withdrawFromAccount(partyAAccount, decimal(50));
           expect(await context.viewFacet.balanceOf(partyAAccount)).to.be.equal(decimal(50));
         });
       });
@@ -161,16 +161,16 @@ export function shouldBehaveLikMultiAccount() {
         beforeEach(async function() {
           const userAddress = await context.signers.user.getAddress();
     
-          await layer.connect(context.signers.user).addAccount("Test");
-          partyAAccount = (await layer.getAccounts(userAddress, 0, 10))[0].accountAddress;
+          await multiAccount.connect(context.signers.user).addAccount("Test");
+          partyAAccount = (await multiAccount.getAccounts(userAddress, 0, 10))[0].accountAddress;
           
           await context.collateral.connect(context.signers.user).mint(userAddress, decimal(510))
           
           await context.collateral
           .connect(context.signers.user)
-          .approve(layer.address, ethers.constants.MaxUint256);
+          .approve(multiAccount.address, ethers.constants.MaxUint256);
           
-          await layer.connect(context.signers.user).depositAndAllocateForAccount(partyAAccount, decimal(500));
+          await multiAccount.connect(context.signers.user).depositAndAllocateForAccount(partyAAccount, decimal(500));
 
         });
     
@@ -179,10 +179,10 @@ export function shouldBehaveLikMultiAccount() {
             ["bytes4", "uint256"],
             [ethers.utils.id("mockFunction(uint256)").slice(0, 10), 123],
           );
-          await expect(layer.connect(context.signers.user2)._call(partyAAccount, [callData])).to.be.reverted;
+          await expect(multiAccount.connect(context.signers.user2)._call(partyAAccount, [callData])).to.be.reverted;
         });
   })
 
-  
+
 })
 }
