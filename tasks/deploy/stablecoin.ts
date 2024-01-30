4dad1e5b256d1816d9c86a0c596edb707fc7f6af
Navid <mojtaba.farokhi99@gmail.com>
2024-01-30 13:54:19 +0200

    Prepare deploy scripts of multiAccount/multicall for testing env

--- tasks/deploy/stablecoin.ts
+++ tasks/deploy/stablecoin.ts
@@ -1,26 +1,26 @@
-import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { task } from "hardhat/config";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { task } from "hardhat/config"
 
 task("verify:stablecoin", "Verifies contract on Etherscan")
   .addParam("address", "The address of the contract to verify")
   .setAction(async ({ address }, { run }) => {
-    await run("verify:verify", {
-      address,
-    });
-  });
+	  await run("verify:verify", {
+		  address,
+	  })
+  })
 
 task("deploy:stablecoin", "Deploys the FakeStablecoin", async (_, { ethers, run }) => {
-  console.log("Running deploy:stablecoin");
+	console.log("Running deploy:stablecoin")
 
-  const signers: SignerWithAddress[] = await ethers.getSigners();
-  const owner: SignerWithAddress = signers[0];
+	const signers: SignerWithAddress[] = await ethers.getSigners()
+	const owner: SignerWithAddress = signers[0]
 
-  const StablecoinFactory = await ethers.getContractFactory("FakeStablecoin");
-  const stablecoin = await StablecoinFactory.connect(owner).deploy();
-  await stablecoin.deployed();
+	const StablecoinFactory = await ethers.getContractFactory("FakeStablecoin")
+	const stablecoin = await StablecoinFactory.connect(owner).deploy()
+	await stablecoin.deployed()
 
-  await stablecoin.deployTransaction.wait();
-  console.log("FakeStablecoin deployed:", stablecoin.address);
+	await stablecoin.deployTransaction.wait()
+	console.log("FakeStablecoin deployed:", stablecoin.address)
 
-  return stablecoin;
-});
+	return stablecoin
+})
