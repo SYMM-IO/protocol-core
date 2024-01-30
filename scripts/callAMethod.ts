5b86bd27bc01107a1d5bcd39d32d07cc6fada16d
Navid <mojtaba.farokhi99@gmail.com>
2024-01-30 15:24:19 +0330

    Prepare deploy scripts of multiAccount/multicall for testing env

--- scripts/callAMethod.ts
+++ scripts/callAMethod.ts
@@ -1,15 +1,15 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 
 async function main() {
-  let v3Address = "0x762407bEd807184F90F3eDcF2D7Ac9CB9d8901c6";
-  let ViewFacet = await ethers.getContractAt("ViewFacet", v3Address);
-  console.log(await ViewFacet.getQuote(3049));
+	let symmioAddress = ""
+	let ViewFacet = await ethers.getContractAt("ViewFacet", symmioAddress)
+	console.log(await ViewFacet.getQuote(564))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch(error => {
-  console.error(error);
-  process.exitCode = 1;
-});
+	console.error(error)
+	process.exitCode = 1
+})
--- scripts/deployPartyB.ts
+++ scripts/deployPartyB.ts
@@ -1,36 +1,45 @@
-import {ethers, run, upgrades} from "hardhat"
+import { ethers, run, upgrades } from "hardhat"
+import { Addresses, loadAddresses, saveAddresses } from "./utils/file"
 
 async function main() {
-    const [deployer] = await ethers.getSigners()
+	const [deployer] = await ethers.getSigners()
 
-    console.log("Deploying contracts with the account:", deployer.address)
+	console.log("Deploying contracts with the account:", deployer.address)
+	let deployedAddresses: Addresses = loadAddresses()
 
-    // Deploy SymmioPartyB as upgradeable
-    const SymmioPartyBFactory = await ethers.getContractFactory("SymmioPartyB")
-    const symmioPartyB = await upgrades.deployProxy(SymmioPartyBFactory, [
-        "", "",
-    ], {initializer: "initialize"})
-    await symmioPartyB.deployed()
+	// Deploy SymmioPartyB as upgradeable
+	const SymmioPartyBFactory = await ethers.getContractFactory("SymmioPartyB")
+	const admin = "0x6ba58CD30014A861b11eD429200Bd1DD8277DCf7"
+	const symmioPartyB = await upgrades.deployProxy(SymmioPartyBFactory, [
+		admin, deployedAddresses.symmioAddress,
+	], { initializer: "initialize" })
+	await symmioPartyB.deployed()
 
-    const addresses = {
-        proxy: symmioPartyB.address,
-        admin: await upgrades.erc1967.getAdminAddress(symmioPartyB.address),
-        implementation: await upgrades.erc1967.getImplementationAddress(
-            symmioPartyB.address,
-        ),
-    }
-    console.log(addresses)
+	const addresses = {
+		proxy: symmioPartyB.address,
+		admin: await upgrades.erc1967.getAdminAddress(symmioPartyB.address),
+		implementation: await upgrades.erc1967.getImplementationAddress(
+		  symmioPartyB.address,
+		),
+	}
+	console.log(addresses)
 
-    await new Promise((r) => setTimeout(r, 15000))
+	deployedAddresses.multiAccountAddress = symmioPartyB.address
+	saveAddresses(deployedAddresses)
 
-    console.log("Verifying contract...")
-    await run("verify:verify", {address: addresses.implementation})
-    console.log("Contract verified!")
+	try {
+		console.log("Verifying contract...")
+		await new Promise((r) => setTimeout(r, 15000))
+		await run("verify:verify", { address: addresses.implementation })
+		console.log("Contract verified!")
+	} catch (e) {
+		console.log(e)
+	}
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+	  console.error(error)
+	  process.exit(1)
+  })
