2e75549e5c74147ef3179b5fe7b505ed23b4a57a
naveed <naveedinno@proton.me>
2024-03-19 10:09:52 +0200

    Add BalanceChange events

---
+++ contracts/libraries/SharedEvents.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: SYMM-Core-Business-Source-License-1.1
+// This contract is licensed under the SYMM Core Business Source License 1.1
+// Copyright (c) 2023 Symmetry Labs AG
+// For more information, see https://docs.symm.io/legal-disclaimer/license
+pragma solidity >=0.8.18;
+
+library SharedEvents {
+
+    enum BalanceChangeType {
+        ALLOCATE,
+        DEALLOCATE,
+        PLATFORM_FEE_IN,
+        PLATFORM_FEE_OUT,
+        REALIZED_PNL_IN,
+        REALIZED_PNL_OUT,
+        CVA_IN,
+        CVA_OUT,
+        LF_IN,
+        LF_OUT
+    }
+
+    event BalanceChangePartyA(address indexed partyA, uint256 amount, BalanceChangeType _type);
+
+    event BalanceChangePartyB(address indexed partyB, address indexed partyA, uint256 amount, BalanceChangeType _type);
+}
