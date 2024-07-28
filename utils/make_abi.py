6b33b52d6883193f77c095e4c04f4d8d9df804c3
naveed <naveedinno@proton.me>
2024-07-28 08:32:19 +0330

    Add contract abis to repo

--- utils/make_abi.py
+++ utils/make_abi.py
@@ -3,23 +3,48 @@ import json
 import os
 import re
 
+
+def scandir(directory_name):
+    sub_folders = [f.path for f in os.scandir(directory_name) if f.is_dir()]
+    for directory_name in list(sub_folders):
+        sub_folders.extend(scandir(directory_name))
+    return sub_folders
+
+
 contract_address = "artifacts/contracts/facets"
+facets_dirs = scandir(contract_address)
+
+
+def remove_duplicates(abi_list):
+    unique_abi = []
+    seen = set()
+    for item in abi_list:
+        # Convert the item to a JSON string for hashing
+        item_str = json.dumps(item, sort_keys=True)
+        if item_str not in seen:
+            seen.add(item_str)
+            unique_abi.append(item)
+    return unique_abi
 
 
 def main():
     abi_data = []
-    for root, dirs, files in os.walk(contract_address):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if re.fullmatch(r'.*\.json', file_path) and not re.fullmatch(r'.*\.dbg\.json', file_path):
-                with open(file_path) as f:
-                    data = json.loads(f.read())
-                    for abi in data['abi']:
-                        if abi not in abi_data:
-                            abi_data.append(abi)
-    abi_data.sort(key=lambda x: x.get('name'))
-    with open('abi.json', 'w') as f:
-        json.dump(abi_data, f, indent=3)
+    for address in facets_dirs:
+        print(f"Checking {address}")
+        file = [f"{address}/{f}" for f in os.listdir(address) if re.fullmatch(".*\.json", f) and
+                not re.fullmatch('.*\.dbg\.json', f)]
+        if len(file) == 0:
+            continue
+        file = file[0]
+        with open(file) as f:
+            data = json.loads(f.read())
+            abi_data += data['abi']
+
+    # Remove duplicates
+    unique_abi_data = remove_duplicates(abi_data)
+
+    with open('abis/symmio.json', 'w') as f:
+        json.dump(unique_abi_data, f, indent=4)
 
 
 if __name__ == '__main__':
