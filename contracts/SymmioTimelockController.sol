6b7daf595b9aa9fd3ae4025bd2f0ff684b0c97a3
naveed <naveedinno@proton.me>
2024-10-07 08:43:27 +0200

    Add timelock contract Fix abi script

---
+++ contracts/SymmioTimelockController.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SYMM-Core-Business-Source-License-1.1
+// This contract is licensed under the SYMM Core Business Source License 1.1
+// Copyright (c) 2023 Symmetry Labs AG
+// For more information, see https://docs.symm.io/legal-disclaimer/license
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract SymmioTimelockController is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(minDelay, proposers, executors, admin) {}
+}
