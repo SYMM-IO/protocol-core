b9d9d09215df2f36acf76c486ad5fedd3956e16d
ZigBalthazar <zigbalthazar@gmail.com>
2025-06-26 13:41:18 +0330

    adapt tests

--- contracts/facets/ViewFacet/ViewFacet.sol
+++ contracts/facets/ViewFacet/ViewFacet.sol
@@ -25,16 +25,16 @@ contract ViewFacet is IViewFacet {
 	 * @return The address of the pendingOwner.
 	 */
 	function pendingOwner() external view virtual returns (address) {
-        return LibDiamond.diamondStorage().pendingOwner;
-    }
+		return LibDiamond.diamondStorage().pendingOwner;
+	}
 
 	/**
 	 * @notice Returns the owner of the diamond.
 	 * @return The address of the owner.
 	 */
 	function owner() external view virtual returns (address) {
-        return LibDiamond.diamondStorage().contractOwner;
-    }
+		return LibDiamond.diamondStorage().contractOwner;
+	}
 
 	/**
 	 * @notice Returns the balance of the specified user.
@@ -918,5 +918,9 @@ contract ViewFacet is IViewFacet {
 	 */
 	function getLiquidationInsuranceVaultParams() external view returns (address, uint256) {
 		return (MAStorage.layout().liquidationInsuranceVault, MAStorage.layout().maxLiquidationProfitPerPosition);
+	}
+
+	function getPartyBWhitelistedSymbolTypeStatus(address partyB, uint256 symbolType) external view returns (bool) {
+		return AccountStorage.layout().partyBWhitelistedSymbolTypes[partyB][symbolType];
 	}
 }
