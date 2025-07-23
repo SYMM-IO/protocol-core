d5862fa9263fc28df0430fae39dc205b1c8517a1
ZigBalthazar <zigbalthazar@gmail.com>
2025-07-23 13:41:09 +0200

    add external transfer

---
+++ contracts/interfaces/IExternalTransferTarget.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SYMM-Core-Business-Source-License-1.1
+// This contract is licensed under the SYMM Core Business Source License 1.1
+// Copyright (c) 2023 Symmetry Labs AG
+// For more information, see https://docs.symm.io/legal-disclaimer/license
+pragma solidity >=0.8.18;
+
+interface IExternalTransferTarget {
+	/**
+	 * @notice Called when an external transfer is initiated from symmio to target and funds are send over there
+	 * @param collateral The collateral token being transferred
+	 * @param sender The address initiating the transfer
+	 * @param receiver The address receiving the transfer
+	 * @param amount The amount of collateral being transferred
+	 */
+	function onTransfer(address collateral, address sender, address receiver, uint256 amount) external;
+}
